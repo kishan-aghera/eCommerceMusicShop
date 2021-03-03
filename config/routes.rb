43039bbd78e7a3c0094fc7136719bbe71fa95052
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-03-03 10:47:00 +0530

    Added funcitonality for cart.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  resources :instruments
   devise_for :users, controllers: { registrations: 'registrations' }
-  root 'store#index'
+  root 'instruments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
